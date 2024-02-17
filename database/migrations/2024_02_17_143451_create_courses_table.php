00aa5809d902177ca8e951b0bfc88088f98a37dc
mazzlookman <mohammadlukmanaqib@gmail.com>
2024-02-17 22:35:39 +0700

    create migrations table & peoject setup

---
+++ database/migrations/2024_02_17_143451_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("certificate");
+            $table->string("thumbnail")->nullable();
+            $table->enum("type", ["free", "premium"]);
+            $table->enum("status", ["draft", "published"]);
+            $table->integer("price")->default(0);
+            $table->enum("level", ["all", "beginner", "intermediate", "advanced"]);
+            $table->longText("description")->nullable();
+            $table->foreignId("mentor_id")->constrained("mentors")->onDelete("cascade");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
